ab51c6da5cef934efc1b4e10644b6892e27ccdd2
Left <vridosh@gmail.com>
2015-03-16 13:28:15 +0300

    Added autofocus flag

--- com.ezartech.ezar/src/android/VideoOverlay.java
+++ com.ezartech.ezar/src/android/VideoOverlay.java
@@ -1,6 +1,7 @@
 package com.ezartech.ezar;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.cordova.CallbackContext;
 
@@ -112,6 +113,12 @@ public class VideoOverlay extends ViewGroup {
         }
 
         Camera.Parameters cameraParameters = camera.getParameters();
+
+        List<String> focusModes = cameraParameters.getSupportedFocusModes();
+        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+        	cameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+        }
+        camera.setParameters(cameraParameters);
 
         if (currentSize == null) {
             setCameraParameters(camera, cameraParameters);
