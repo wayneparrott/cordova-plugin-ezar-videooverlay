c6b7fb8e0b7862398a0fb9e38234c2ca2a7d23e5
Wayne Parrott <wayne@ezartech.com>
2016-02-10 11:16:45 -0600

    change api for getDirection()

--- src/android/CameraDirection.java
+++ src/android/CameraDirection.java
@@ -16,7 +16,7 @@ import android.hardware.Camera.CameraInfo;
 
 public enum CameraDirection {
 	BACK {
-		public int getCameraInfoFacing() {
+		public int getDirection() {
 			return Camera.CameraInfo.CAMERA_FACING_BACK;
 		}
 		
@@ -25,7 +25,7 @@ public enum CameraDirection {
 		}
 	},
 	FRONT {
-		public int getCameraInfoFacing() {
+		public int getDirection() {
 			return Camera.CameraInfo.CAMERA_FACING_FRONT;
 		}		
 		
@@ -35,7 +35,7 @@ public enum CameraDirection {
 	}
 	;
 
-	public abstract int getCameraInfoFacing();
+	public abstract int getDirection();
 	public abstract boolean isFlipping();
 
 	public static CameraDirection fromCameraInfo(CameraInfo info) {
